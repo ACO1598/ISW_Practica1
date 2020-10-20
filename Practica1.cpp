98aeaac6434c077fb81d906cec53c5cc979de684
Alan Clech <alanclecholiveros@gmail.com>
2020-10-20 10:41:00 +0200

    Creación del menu

--- Practica1.cpp
+++ Practica1.cpp
@@ -11,5 +11,21 @@ using namespace std;
 
 int main() {
 	//este va a ser el menu para nuestro programa
+	std::cout << "Por favor introduzca que es lo que desea hacer" << '\n';
+	std::cout << "1: Introducir los vectores con los que desea trabajar" << '\n';
+	std::cout << "2: Suma de vectores" << '\n';
+	std::cout << "3: Resta de vectores" << '\n';
+
+	int temp;
+	std::cin >> temp;
+
+	if (temp==1) {
+		//ejecutar la introduccion de vectores
+	} else if (temp==2) {
+		//ejecutar suma de vectores
+	} else if(temp==3){
+		//ejecutar resta de vectores
+	}
+
 	return 0;
 }
