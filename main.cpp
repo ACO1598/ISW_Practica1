3c172d30806c552369912493576e60dac1b6ce0d
amorenotalero <amorenotalero@al.uloyola.es>
2020-10-20 10:38:45 +0200

    Añadido comentario

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ int main(int argc, char const *argv[])
 	vector<int> v1 = {1,2,3};
 	vector<int> v2 = {2,1,9};
 	vector<int> v3 = sumaVectores(v1,v2);
+	/*codigo añadido para mostrar por pantalla el resultado se puede borrar*/
 	for(int i : v3)
 	{
 		cout << i << endl;
