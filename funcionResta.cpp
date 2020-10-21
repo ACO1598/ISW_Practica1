4d62296d15bb48b095bbd8eebdbe0a5742ed4698
Pedro Blasco España <Pedro Blasco Espaa>
2020-10-21 12:40:20 +0200

    insercion

---
+++ funcionResta.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector> 
+#include "funciones.h"
+
+vector<int> restaVectores(vector<int> v1, vector<int> v2)
+{
+	if (v1.size() == v2.size())
+	{
+		int N;
+		N = v1.size();
+		for (int i = 0; i < N; i++)
+		{
+			resta[i] = v1[i] - v2[i];
+		}
+	}
+	else {
+		cout << "vectores de diferente tamaño"
+	}
+	return resta
+}
