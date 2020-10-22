9fa07daa10b0925602a82575c6010607bd3227e6
Alan Clech <alanclecholiveros@gmail.com>
2020-10-22 20:03:27 +0200

    Funcionalidad de introducir vectores y refinamiento de menu

--- Main.cpp
+++ Main.cpp
@@ -9,20 +9,38 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include "funciones.h"
 using namespace std;
+
+vector<int> introVectores(){
+
+	printf("Introduzca el numero de elementos de su vector\n");
+	int n;
+	std::cin >> n;
+	std::vector<int> v (n, 0);
+	
+	int i=0;
+	while (i!= n) {
+		int temp;
+		std::cout << "Introduzca el valor de la posicion " << i << '\n';
+		std::cin >> temp;
+		v[i] = temp;
+		i++;
+	}
+
+	return v;
+}
 
 int main() {
 	//este va a ser el menu para nuestro programa
-	std::cout << "Por favor introduzca que es lo que desea hacer" << '\n';
-	std::cout << "1: Introducir los vectores con los que desea trabajar" << '\n';
-	std::cout << "2: Suma de vectores" << '\n';
-	std::cout << "3: Resta de vectores" << '\n';
-	std::cout << "4: Salir del programa" << '\n';
-
 	int temp;
 
 	while (temp != 4) {
+		std::cout << "Por favor introduzca que es lo que desea hacer" << '\n';
+		std::cout << "1: Introducir los vectores con los que desea trabajar" << '\n';
+		std::cout << "2: Suma de vectores" << '\n';
+		std::cout << "3: Resta de vectores" << '\n';
+		std::cout << "4: Salir del programa" << '\n';
+
 		std::cin >> temp;
 		std::vector<int> v1;
 		std::vector<int> v2;
@@ -31,6 +49,8 @@ int main() {
 			//ejecutar la introduccion de vectores
 			std::cout << "opcion 1" << '\n';
 			v1= introVectores();
+			printf("Introduzca el segundo vector \n");
+			v2= introVectores();
 		} else if (temp==2) {
 			std::cout << "opcion 2" << '\n';
 			//vector<int> v3= sumaVectores(v1, v2);
@@ -41,7 +61,6 @@ int main() {
 			std::cout << "Hasta luego" << '\n';
 		}
 	}
-
 
 	return 0;
 }
--- funciones.h
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
-vector<int> sumaVectores(vector<int> v1, vector<int> v2);
-
-vector<int> introVectores();
